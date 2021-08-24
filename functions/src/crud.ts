8ae24b0ac5d90b045f216cd33ef91c71bf5ec0d0
mbanov <m.banov7@gmail.com>
2021-08-24 09:55:10 +0200

    add recursive subcollection option

--- functions/src/crud.ts
+++ functions/src/crud.ts
@@ -5,7 +5,6 @@ import cors = require("cors");
 import { validateCrudOperations } from "./auth";
 import { getSubcollectionFromDoc, queryValue } from "./utils";
 
-
 export const crudOperations = (collectionPath: string): Express => {
   const app = express();
   app.use(cors());
@@ -14,111 +13,128 @@ export const crudOperations = (collectionPath: string): Express => {
 
   app.get("/", (req, res) => {
     let snapshot: FirebaseFirestore.Query<FirebaseFirestore.DocumentData> =
-        db.collection(collectionPath);
+      db.collection(collectionPath);
 
     for (let [fieldPath, value] of Object.entries(req.query)) {
-        snapshot = snapshot.where(fieldPath, "==", queryValue(value));
+      snapshot = snapshot.where(fieldPath, "==", queryValue(value));
     }
 
-    snapshot.get()
-        .then((querySnapshot) => {
-          const arr = [];
+    snapshot
+      .get()
+      .then(async (querySnapshot) => {
+        const arr = [];
 
-          for (const doc of querySnapshot.docs) {
-            arr.push({ id: doc.id, ...doc.data() });
-           // arr.push(getDocWithSubcollections(doc))
-          }
+        for (const doc of querySnapshot.docs) {
+          arr.push({ id: doc.id, ...doc.data() });
+          // arr.push(await getDocWithSubcollections(doc));
+          // arr.push(await getDocWithSubcollectionsRecursive(doc));
+        }
 
-          console.log("GET success");
-          res.send(arr);
-        })
-        .catch((error) => {
-          console.error("GET error", error);
-          res.status(500).send(error);
-        });
+        console.log("GET success");
+        res.send(arr);
+      })
+      .catch((error) => {
+        console.error("GET error", error);
+        res.status(500).send(error);
+      });
   });
 
   app.get("/:id", (req, res) => {
-    db.collection(collectionPath).doc(req.params.id).get()
-        .then((doc) => {
-          if (doc.exists) {
-            console.log("GET success", req.params.id);
-            res.send(doc.data());
-          } else {
-            console.log("GET undefined", req.params.id);
-            res.status(404).send("Not found");
-          }
-        }).catch((error) => {
-          console.error("GET error", error);
-          res.status(500).send(error);
-        });
+    db.collection(collectionPath)
+      .doc(req.params.id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          console.log("GET success", req.params.id);
+          res.send(doc.data());
+        } else {
+          console.log("GET undefined", req.params.id);
+          res.status(404).send("Not found");
+        }
+      })
+      .catch((error) => {
+        console.error("GET error", error);
+        res.status(500).send(error);
+      });
   });
 
   app.get("/:id/:subcollection", (req, res) => {
-    db.collection(collectionPath).doc(req.params.id).get()
-        .then(async (doc) => {
-          if (doc.exists) {
-            const result = await getSubcollectionFromDoc(doc, req.params.subcollection);
-            console.log("GET success", req.params.id);
-            res.send(result);
-          } else {
-            console.log("GET undefined", req.params.id);
-            res.status(404).send("Not found");
-          }
-        }).catch((error) => {
+    db.collection(collectionPath)
+      .doc(req.params.id)
+      .get()
+      .then(async (doc) => {
+        if (doc.exists) {
+          const result = await getSubcollectionFromDoc(
+            doc,
+            req.params.subcollection
+          );
+          console.log("GET success", req.params.id);
+          res.send(result);
+        } else {
+          console.log("GET undefined", req.params.id);
+          res.status(404).send("Not found");
+        }
+      })
+      .catch((error) => {
         console.error("GET error", error);
         res.status(500).send(error);
-    });
+      });
   });
 
   app.post("/", (req, res) => {
-    db.collection(collectionPath).add(req.body)
-        .then((doc) => {
-          console.log("POST success", doc.id);
-            res.status(200).send("Successfully added");
-        })
-        .catch((error) => {
-          console.error("POST error", error);
-          res.status(500).send(error);
-        });
+    db.collection(collectionPath)
+      .add(req.body)
+      .then((doc) => {
+        console.log("POST success", doc.id);
+        res.status(200).send("Successfully added");
+      })
+      .catch((error) => {
+        console.error("POST error", error);
+        res.status(500).send(error);
+      });
   });
 
   app.post("/:id/:subcollection", (req, res) => {
-      db.collection(collectionPath).doc(req.params.id)
-          .collection(req.params.subcollection).add(req.body)
-          .then((doc) => {
-              console.log("POST success", doc.id);
-              res.status(200).send("Successfully added");
-          })
-          .catch((error) => {
-              console.error("POST error", error);
-              res.status(500).send(error);
-          });
+    db.collection(collectionPath)
+      .doc(req.params.id)
+      .collection(req.params.subcollection)
+      .add(req.body)
+      .then((doc) => {
+        console.log("POST success", doc.id);
+        res.status(200).send("Successfully added");
+      })
+      .catch((error) => {
+        console.error("POST error", error);
+        res.status(500).send(error);
+      });
   });
 
   app.put("/:id", (req, res) => {
-    db.collection(collectionPath).doc(req.params.id)
-        .set(req.body)
-        .then((doc) => {
-          console.log("PUT success", req.params.id);
-          res.status(200).send("Successfully updated");
-        })
-        .catch((error) => {
-          console.error("PUT error", error);
-          res.status(500).send(error);
-        });
+    db.collection(collectionPath)
+      .doc(req.params.id)
+      .set(req.body)
+      .then((_) => {
+        console.log("PUT success", req.params.id);
+        res.status(200).send("Successfully updated");
+      })
+      .catch((error) => {
+        console.error("PUT error", error);
+        res.status(500).send(error);
+      });
   });
 
   app.delete("/:id", (req, res) => {
-    db.collection(collectionPath).doc(req.params.id).delete()
-        .then((doc) => {
-          console.log("DELETE success", req.params.id);
-          res.status(200).send("Successfully deleted");
-        })
-        .catch((error) => {
-          console.error("DELETE error", error);
-          res.status(500).send(error);
-        });
+    db.collection(collectionPath)
+      .doc(req.params.id)
+      .delete()
+      .then((_) => {
+        console.log("DELETE success", req.params.id);
+        res.status(200).send("Successfully deleted");
+      })
+      .catch((error) => {
+        console.error("DELETE error", error);
+        res.status(500).send(error);
+      });
   });
 
   return app;
--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -1,55 +1,75 @@
 import * as admin from "firebase-admin";
 
-const WHITELIST = [
-    '^(surveys/\\w+/results)$',
-];
+const WHITELIST = ["^(surveys/\\w+/results)$"];
 
 export const queryValue = (value: any) => {
-    switch(value) {
-        case "true":
-            return true;
-        case "false":
-            return false;
-        default:
-            return value;
-    }
-}
+  switch (value) {
+    case "true":
+      return true;
+    case "false":
+      return false;
+    default:
+      return value;
+  }
+};
 
 export const getUserFavorites = async (favoritesIdList: any[]) => {
-    const favorites = [];
-    for (const { favoriteId, isEvent } of favoritesIdList) {
-        const collectionPath = isEvent ? "events" : "surveys";
-        try {
-            const f = await admin.firestore().collection(collectionPath).doc(favoriteId).get();
-            favorites.push(f.data())
-        } catch (error) {
-            return { status: 500, favorites: error }
-        }
+  const favorites = [];
+  for (const { favoriteId, isEvent } of favoritesIdList) {
+    const collectionPath = isEvent ? "events" : "surveys";
+    try {
+      const f = await admin
+        .firestore()
+        .collection(collectionPath)
+        .doc(favoriteId)
+        .get();
+      favorites.push(f.data());
+    } catch (error) {
+      return { status: 500, favorites: error };
     }
-    return { status: 200, favorites };
-}
+  }
+  return { status: 200, favorites };
+};
 
 export const getDocWithSubcollections = async (doc: any) => {
-    const item: any = { id: doc.id, ...doc.data() };
-    const subcollections = await doc.ref.listCollections();
-    for (const sub of subcollections) {
-        item[sub.id] = (await sub.get()).docs.map((s: any) => ({
-            id: s.id, ...s.data()
-        }));
-    }
-    return item;
-}
+  const item: any = { id: doc.id, ...doc.data() };
+  const subcollections = await doc.ref.listCollections();
+  for (const sub of subcollections) {
+    item[sub.id] = (await sub.get()).docs.map((s: any) => ({
+      id: s.id,
+      ...s.data(),
+    }));
+  }
+  return item;
+};
 
-export const getSubcollectionFromDoc = async (doc: any, subcollection: string) => {
-    const sub = await doc.ref.collection(subcollection).get();
-    return sub.docs.map((s: any) => ({ id: s.id, ...s.data() }));
-}
+export const getDocWithSubcollectionsRecursive = async (doc: any) => {
+  const item: any = { id: doc.id, ...doc.data() };
+  const subcollections = await doc.ref.listCollections();
+  for (const sub of subcollections) {
+    const arr = [];
+    const subDocs = (await sub.get()).docs;
+    for (const d of subDocs) {
+      arr.push(await getDocWithSubcollectionsRecursive(d));
+    }
+    item[sub.id] = arr;
+  }
+  return item;
+};
+
+export const getSubcollectionFromDoc = async (
+  doc: any,
+  subcollection: string
+) => {
+  const sub = await doc.ref.collection(subcollection).get();
+  return sub.docs.map((s: any) => ({ id: s.id, ...s.data() }));
+};
 
 export const isPathWhitelisted = (path: string) => {
-    for (const regex of WHITELIST) {
-        if (RegExp(regex).test(path)) {
-            return true;
-        }
+  for (const regex of WHITELIST) {
+    if (RegExp(regex).test(path)) {
+      return true;
     }
-    return false;
-}
+  }
+  return false;
+};
