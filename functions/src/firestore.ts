eedb619d7900049ea037d94cd50e6b87ec66fcc7
mbanov <m.banov7@gmail.com>
2021-09-03 15:31:02 +0200

    send notification when survey is published

--- functions/src/firestore.ts
+++ functions/src/firestore.ts
@@ -1,24 +1,45 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-export const deleteSubcollections =
-  functions.firestore.document("{collection}/{id}")
-      .onDelete(async (snapshot, context) => {
-        const subcollections = await snapshot.ref.listCollections();
-        if (subcollections.length === 0) {
-          return;
-        }
+export const deleteSubcollections = functions.firestore
+  .document("{collection}/{id}")
+  .onDelete(async (snapshot, _) => {
+    const subcollections = await snapshot.ref.listCollections();
+    if (subcollections.length === 0) {
+      return;
+    }
 
-        const docs = [];
-        for (const s of subcollections) {
-          const res = await s.listDocuments();
-          docs.push(...res);
-        }
-        console.log(`Found ${subcollections.length} subcollections` +
-                    ` with ${docs.length} documents\n` +
-                    "Deleting...");
+    const docs = [];
+    for (const s of subcollections) {
+      const res = await s.listDocuments();
+      docs.push(...res);
+    }
+    console.log(
+      `Found ${subcollections.length} subcollections with ${docs.length} documents\n Deleting...`
+    );
 
-        const batch = admin.firestore().batch();
-        docs.map((doc) => batch.delete(doc));
-        return batch.commit();
-      });
+    const batch = admin.firestore().batch();
+    docs.map((doc) => batch.delete(doc));
+    return batch.commit();
+  });
+
+export const notifySurveyPublished = functions.firestore
+  .document("surveys/{id}")
+  .onUpdate(async (change, _) => {
+    const before = change.before.data();
+    const after = change.after.data();
+    if (!before?.published && after?.published) {
+      try {
+        await admin.messaging().send({
+          topic: change.after.id,
+          notification: {
+            title: "Objavljeni su rezultati ankete!",
+            body: `Pogledaj najnovije rezultate ankete "${after.title}"`,
+          },
+        });
+        console.log("SEND NOTIFICATION success");
+      } catch (error) {
+        console.error("SEND NOTIFICATION error", error);
+      }
+    }
+  });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,7 @@ admin.initializeApp();
 export const newUserSignUp = user.newUserSignUp;
 export const userDeleted = user.userDeleted;
 export const deleteSubcollections = firestore.deleteSubcollections;
+export const notifySurveyPublished = firestore.notifySurveyPublished;
 export const enums = functions.https.onRequest(crud.generics("enums"));
 export const events = functions.https.onRequest(crud.generics("events"));
 export const surveys = functions.https.onRequest(crud.surveys());
